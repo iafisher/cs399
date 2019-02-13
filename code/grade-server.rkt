049cb744bff3a73f503e96e8208e39937e4bef18
Ian Fisher <iafisher@protonmail.com>
2019-02-13 08:37:06 -0500

    First try at grade server

--- code/grade-server.rkt
+++ code/grade-server.rkt
@@ -8,7 +8,7 @@
 ;
 ; Professors may see the grades of every student for all assignments. TAs may see the
 ; grades of every student, but only for the last assignment. Students may only see their
-; own grades.
+; own individual grades, but they may see the average grade of everyone in the class.
 
 ; In order to run this module, you need to copy racets.rkt, racets-mlang.rkt, and
 ; facets.rkt from https://github.com/fordsec/racets
@@ -18,14 +18,41 @@
   (lambda (player-name)
     (let-label l (lambda (x) (equal? x player-name)) l)))
 
-(struct student (name policy grade-list))
+;(struct student (name policy grade-list))
 
-(define student-with-policy
-  (lambda (name grade-list) (student name (make-policy name) grade-list)))
+;(define student-with-policy
+;    (lambda (name grade-list)
+;      (let ([policy (make-policy name)])
+;	(student name policy (fac policy grade-list '())))))
 
 ; Some hard-coded data
-(define alice (student-with-policy "Alice" '(93 84 87)))
-(define bob (student-with-policy "Bob" '(84 71 73)))
+;(define alice (student-with-policy "Alice" '(93 84 87)))
+;(define bob (student-with-policy "Bob" '(84 71 73)))
+;(define students (list alice bob))
+(define alice-policy (make-policy "Alice"))
+(define bob-policy (make-policy "Bob"))
+
+(define alice-grades (fac alice-policy '(93 84 87) '()))
+(define bob-grades (fac bob-policy '(84 71 73) '()))
 
 
-(display "Nothing implemented yet!\n")
+;(define print-grades
+;  (lambda (arg student)
+;    (displayln (obs (student-policy student) arg (student-grade-list student)))))
+
+(define print-alice-grades
+  (lambda (arg)
+    (displayln (obs alice-policy arg alice-grades))))
+
+(define print-bob-grades
+  (lambda (arg)
+    (displayln (obs bob-policy arg bob-grades))))
+
+; How do I implement this?
+(define print-average-grade (lambda (student-list) 0))
+
+(display "Alice's grades, viewed by Alice: ")
+(print-alice-grades "Alice")
+
+(display "Alice's grades, viewed by Bob: ")
+(print-alice-grades "Bob")
