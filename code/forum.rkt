65631139dc70a33c3bf6f98bb55f16d5c8b1380b
Ian Fisher <iafisher@protonmail.com>
2019-02-27 12:46:36 -0500

    More work on forum application

--- code/forum.rkt
+++ code/forum.rkt
@@ -12,15 +12,32 @@
 
 
 (struct User (name policy karma is-admin))
-(struct Post (user text score flagged))
+(struct Post (username text score flagged))
 
 
 (define user-list-policy
   (let-label l
 	     (lambda (username)
-	       (let ([user (get-user username)])
-		 (or (User-is-admin user) (>= (User-karma user) 100))))
+	       (if (equal? username "root")
+		 #t
+		 (let ([user (get-user username)])
+		   (and user (or (User-is-admin user) (>= (User-karma user) 100))))))
 	     l))
+
+(define admin-policy
+  (let-label l
+	     (lambda (username)
+	       (if (equal? username "root")
+		 #t
+		 (let ([user (get-user username)])
+		   (and user (User-is-admin user)))))
+	     l))
+
+(define user-policy
+  (let-label l
+	     (lambda (username) (or (equal? username "root") (get-user username)))
+	     l))
+
 
 ; Make a policy for a particular user.
 (define (make-policy username)
@@ -30,26 +47,50 @@
 	     l))
 
 
+(define (make-user name karma is-admin) (User name (make-policy name) karma is-admin))
+(define (make-post username text score flagged)
+  (fac admin-policy
+       (Post username text score flagged)
+       (fac user-policy
+	    (Post username text score #f)
+	    (Post username text 0 #f))))
+
+
 ; Some sample users.
-(define alice (User "Alice" (make-policy "Alice") 2000 #f))
-(define bob (User "Bob" (make-policy "Bob") 50 #t))
-(define eve (User "Eve" (make-policy "Eve") 12 #f))
+(define alice (make-user "Alice" 2000 #f))
+(define bob (make-user "Bob" 50 #t))
+(define eve (make-user "Eve" 12 #f))
 
 
 (define users (fac user-list-policy (list alice bob eve) '()))
-(define posts '())
+(define posts (list (make-post "Alice" "Hello everybody!" 23 #t)))
 
 
 (define (get-user username)
   (define (get-user-rec username lst)
     (if (empty? lst)
       #f
-      (if (equal? username (car lst))
+      (if (equal? username (User-name (car lst)))
 	(car lst)
 	(get-user-rec username (cdr lst)))))
-  (get-user-rec (obs user-list-policy "root" users)))
+  (get-user-rec username (obs user-list-policy "root" users)))
+
+(define (get-post post-id)
+
 
 
 ;;; The mock API endpoints. ;;;
 (define (print-post username post-id)
   (displayln "Not implemented yet!"))
+
+(define (print-user-list username)
+  (displayln (obs user-list-policy username users)))
+
+
+; Some sample API calls.
+(display "User list, observed by Alice: ")
+(print-user-list "Alice")
+(display "User list, observed by Bob: ")
+(print-user-list "Bob")
+(display "User list, observed by Eve: ")
+(print-user-list "Eve")
